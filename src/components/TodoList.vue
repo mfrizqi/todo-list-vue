0730cc11118b31283b3e4d20d9474a5270e77048
mfrizqi <muhammadfajarrizqi@gmail.com>
2025-04-02 05:17:05 +0800

    Fix draggable index items ids

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,49 +3,115 @@
     <div class="w-full max-w-[480px]">
       <h1 class="text-3xl font-bold text-center mb-20">Todo List</h1>
       <div class="flex flex-cols justify-center mb-16">
-        <input type="text" name="todo-input" id="todo_input"
+        <input
+          type="text"
+          name="todo-input"
+          id="todo_input"
           class="border border-neutral-500 mr-2 rounded-md py-2 px-4 focus:outline-none focus:border-mountain-meadow-500 w-full"
-          v-model="todo.name" />
-        <button @click="handleAdd"
+          v-model="todo.name"
+        />
+        <button
+          @click="handleAdd"
           class="px-4 py-3 bg-mountain-meadow-600 hover:shadow rounded-md text-white cursor-pointer disabled:opacity-60"
-          :disabled="todo.name.length === 0">
+          :disabled="todo.name.length === 0"
+        >
           Add
         </button>
       </div>
       <section>
-        <VueDraggable v-model="todos.list" class="flex flex-col gap-2 w-300px rounded py-4" target=".sort-target"
-          :scroll="true" :disabled="disabled" :animation="150" ghostClass="ghost">
-          <TransitionGroup type="transition" tag="ul" name="fade" class="sort-target"
-            @before-enter="disableDelete = true" @after-leave="disableDelete = false">
-            <li v-for="(item, index) in todos.list" :key="`${item.id}_${index}`"
+        <VueDraggable
+          v-model="todos.list"
+          class="flex flex-col gap-2 w-300px rounded py-4"
+          target=".sort-target"
+          :scroll="true"
+          :disabled="disabled"
+          :animation="150"
+          ghostClass="ghost"
+        >
+          <TransitionGroup
+            type="transition"
+            tag="ul"
+            name="fade"
+            class="sort-target"
+            @before-enter="disableDelete = true"
+            @after-leave="disableDelete = false"
+          >
+            <li
+              v-for="(item, index) in todos.list"
+              :key="item.id"
               class="h-50px bg-white shadow-md rounded flex justify-between p-2 py-3 mb-3 rounded-lg"
-              :class="{ 'items-start': todoOption === item.id, 'items-center': todoOption !== item.id }">
-              <div class="flex grow mr-2 cursor-move"
-                :class="{ 'items-start': todoOption === item.id, 'items-center': todoOption !== item.id }">
-                <div class="flex items-center mr-4"
-                  :class="{ 'mt-1': todoOption === item.id, 'mt-0': todoOption !== item.id }">
-                  <VueFeather type="menu" class="text-neutral-500 w-5 h-5"></VueFeather>
+              :class="{
+                'items-start': todoOption === item.id,
+                'items-center': todoOption !== item.id,
+              }"
+            >
+              <div
+                class="flex grow mr-2 cursor-move"
+                :class="{
+                  'items-start': todoOption === item.id,
+                  'items-center': todoOption !== item.id,
+                }"
+              >
+                <div
+                  class="flex items-center mr-4"
+                  :class="{
+                    'mt-1': todoOption === item.id,
+                    'mt-0': todoOption !== item.id,
+                  }"
+                >
+                  <VueFeather
+                    type="menu"
+                    class="text-neutral-500 w-5 h-5"
+                  ></VueFeather>
                 </div>
-                <input v-if="false" type="checkbox" name="todo-checks" v-model="item.check"
-                  class="mr-2 appearance-none border-neutral-500 border w-4 h-4 rounded-sm bg-white checked:bg-blue-800 checked:border-0" />
-                <div v-if="todoOption !== item.id" class="text-zinc-700">{{ item.name }}</div>
+                <input
+                  v-if="false"
+                  type="checkbox"
+                  name="todo-checks"
+                  v-model="item.check"
+                  class="mr-2 appearance-none border-neutral-500 border w-4 h-4 rounded-sm bg-white checked:bg-blue-800 checked:border-0"
+                />
+                <div v-if="todoOption !== item.id" class="text-zinc-700">
+                  {{ item.name }}
+                </div>
                 <div v-if="todoOption === item.id">
                   <label class="text-sm text-neutral-500">Title</label>
-                  <input type="text" v-model="item.name" @blur="handleEdit(false)" @keydown="handleEdit(true)"
-                    class="border border-neutral-400 rounded-md py-1 px-3 w-full focus:outline-none focus:border-mountain-meadow-500 mb-3 mt-1 text-zinc-600" />
+                  <input
+                    type="text"
+                    v-model="item.name"
+                    @blur="handleEdit(false)"
+                    @keydown="handleEdit(true)"
+                    class="border border-neutral-400 rounded-md py-1 px-3 w-full focus:outline-none focus:border-mountain-meadow-500 mb-3 mt-1 text-zinc-600"
+                  />
                   <label class="text-sm text-neutral-500">Detail</label>
-                  <textarea v-model="item.description" rows="2"
-                    class="w-full border border-neutral-400 rounded-sm focus:outline-none focus:border-mountain-meadow-500  py-1 px-3 mt-1 text-zinc-600"
-                    @blur="handleEdit(false)" @keydown="handleEdit(true)" placeholder="Add detail"></textarea>
+                  <textarea
+                    v-model="item.description"
+                    rows="2"
+                    class="w-full border border-neutral-400 rounded-sm focus:outline-none focus:border-mountain-meadow-500 py-1 px-3 mt-1 text-zinc-600"
+                    @blur="handleEdit(false)"
+                    @keydown="handleEdit(true)"
+                    placeholder="Add detail"
+                  ></textarea>
                 </div>
               </div>
-              <div @click="todos.delete(index)" class="flex items-center mr-2 cursor-pointer text-neutral-500 hover:text-white hover:bg-rose-500 rounded-md p-1">
+              <div
+                @click="todos.delete(index)"
+                class="flex items-center mr-2 cursor-pointer text-neutral-500 hover:text-white hover:bg-rose-500 rounded-md p-1"
+              >
                 <VueFeather type="trash-2" class="w-5 h-5"></VueFeather>
               </div>
               <div @click="toggleOpen(item)" class="flex cursor-pointer">
-                <VueFeather type="chevron-down" class="text-neutral-500 w-5 h-5" v-if="todoOption !== item.id">
+                <VueFeather
+                  type="chevron-down"
+                  class="text-neutral-500 w-5 h-5"
+                  v-if="todoOption !== item.id"
+                >
                 </VueFeather>
-                <VueFeather type="chevron-up" class="text-neutral-500 w-5 h-5" v-if="todoOption === item.id">
+                <VueFeather
+                  type="chevron-up"
+                  class="text-neutral-500 w-5 h-5"
+                  v-if="todoOption === item.id"
+                >
                 </VueFeather>
               </div>
             </li>
@@ -65,65 +131,64 @@ import { onMounted, ref } from "vue";
 
 import VueFeather from "vue-feather";
 import { VueDraggable } from "vue-draggable-plus";
-import { uuid } from 'vue-uuid';
+import { uuid } from "vue-uuid";
 
 import { useTodoStore } from "../stores/todo";
 
-const todos = useTodoStore()
+const todos = useTodoStore();
 
 onMounted(() => {
-  todos.initValue()
-  console.log(todos.list)
-})
+  todos.initValue();
+  console.log(todos.list);
+});
 
 let todo = ref({
-  name: '',
-  description: '',
-  check: '',
-  id: '',
+  name: "",
+  description: "",
+  check: "",
+  id: "",
   option: {
     edit: false,
-    open: false
+    open: false,
   },
-})
+});
 
-let disabled = ref(false)
-let disableDelete = ref(false)
-let todoOption = ref(null)
+let disabled = ref(false);
+let disableDelete = ref(false);
+let todoOption = ref(null);
 
 function handleAdd() {
-  todo.value.id = uuid.v4()
-  todos.add(todo)
+  todo.value.id = uuid.v4();
+  todos.add(todo);
   setTimeout(() => {
     todo.value = {
-      name: '',
-      description: '',
-      check: '',
-      id: '',
+      name: "",
+      description: "",
+      check: "",
+      id: "",
       option: {
         edit: false,
-        open: false
+        open: false,
       },
-    }
+    };
   }, 10);
 }
 
 function handleEdit(isDebounce) {
-  let time = isDebounce ? 500 : 0
+  let time = isDebounce ? 500 : 0;
   setTimeout(() => {
-    todos.edit(todos.list)
+    todos.edit(todos.list);
   }, time);
 }
 
 function toggleOpen(item) {
   console.log(item);
-  console.log(todoOption)
-  if (!todoOption.value) {
-    todoOption.value = item.id
-  } else {
-    todoOption.value = null
-  }
-
+  console.log(todoOption);
+  if (!todoOption.value || todoOption.value !== item.id) {
+    todoOption.value = item.id;
+  } else if(todoOption.value === item.id) {
+    todoOption.value = null;
+  } 
 }
 </script>
 
@@ -150,6 +215,6 @@ function toggleOpen(item) {
 }
 
 textarea {
-  resize: none
+  resize: none;
 }
 </style>
